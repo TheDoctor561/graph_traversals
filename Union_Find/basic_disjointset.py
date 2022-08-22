44b99067a955a71d346f60869f538c44fd210404
Daniel Yu-cua <daniel.yucua@gmail.com>
2022-08-22 03:27:33 -0400

    basic disjoint

--- Union_Find/basic_disjointset.py
+++ Union_Find/basic_disjointset.py
@@ -14,9 +14,31 @@
 
 class UnionFind: 
     def __init__(self, size):
-        self.arr = [i for i in range(size)]
+        self.root = [i for i in range(size)]
+    
+    # union connects two nodes with a left bias meaning that b will always 
+    # connect to a 
+    def union(self,a,b): 
+        # Basically we want to take the parent node of the given thing
+        fa = self.find(a)
+        fb = self.find(b)
+        if fa != fb: 
+            self.root[fa] = fb
 
+    # takes in a node a and returns the root node of such
+    def find(self, a): 
+        while a != self.root[a]: 
+            a = self.root[a]
+        return a 
+        
+uu = UnionFind(10)
 
-uu = UnionFind(5)
-
-print(uu.arr)
+print(uu.root)
+uu.union(0,1)
+uu.union(0,2)
+uu.union(1,3)
+uu.union(4,8)
+uu.union(5,6)
+uu.union(5,7)
+print(uu.root)
+print([i for i in range(10)])
